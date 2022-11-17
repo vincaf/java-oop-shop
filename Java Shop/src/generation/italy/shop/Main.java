1187c9527c8efc1d28567c7f28f5ef1da9590c6f
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-17 15:22:01 +0100

    feat: create main class & add new Prodotto

---
+++ Java Shop/src/generation/italy/shop/Main.java
@@ -0,0 +1,12 @@
+package generation.italy.shop;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Prodotto product1 = new Prodotto("Computer portatile", "Notebook PC Portatile da 15,6 Pollici", 300);
+		System.out.println(product1.toString());
+
+	}
+
+}
