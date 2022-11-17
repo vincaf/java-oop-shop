920c2c8bb0c1a6f414434170c39252ea946540d9
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-17 15:27:12 +0100

    full object representation in main, completed

--- Java Shop/src/generation/italy/shop/Main.java
+++ Java Shop/src/generation/italy/shop/Main.java
@@ -6,6 +6,10 @@ public class Main {
 
 		Prodotto product1 = new Prodotto("Computer portatile", "Notebook PC Portatile da 15,6 Pollici", 300);
 		System.out.println(product1.toString());
+		
+		int product1Price = product1.getPrice();
+		int product1Iva = product1.getPriceIva();
+		System.out.println("\nPrezzo: " + product1Price + "€ - Prezzo con IVA: " + product1Iva + "€");
 
 	}
 
